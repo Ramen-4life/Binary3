0f9ca575f9132ece20ee53a79b6e890c7cb6b657
Ramen-4life <quincyalex09@gmail.com>
2024-09-17 08:42:53 -0500

    Initial Commit

---
+++ Binary/BinaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryApp.swift
+//  Binary
+//
+//  Created by Quincy Alex on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BinaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
